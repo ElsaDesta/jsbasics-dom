8f4ca4fa793765a19294776836352e578d711620
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-01-22 16:35:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/* 1. Create 2 arrays with objects for the pets. */
+
+/* 2. Join the arrays with either .concat() or spread operator */
+
+/* 3. Retrieve and store the 'pet-list' and 'pet-select' elements from the HTML and store them in variables. */
+
+/* 4. Create a function that will display the pets. The function should have:
+    - A parameter to represent the array that will be passed.
+    - A loop that retrieves each object of the array.
+    - New li, p, img elements that are created using .createElement('nameOfHTMLElement').
+      - Values of the array object should be assigned to p, img elements respectively.
+      - Append all the newly created elements to the ul with the id of 'pet-list'.
+*/
+
+/* 5. An eventListener for the select and apply the necessary conditions. */
